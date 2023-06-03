b6eeacdfc724920bc76ed838400e7a6ce5043048
Miguel Martelo <miguemartelo1031@gmail.com>
2023-06-03 15:06:22 -0500

    feat: add update user profile method

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -71,14 +71,34 @@ const getUserProfile = asyncHandler(async (req, res) => {
     name: req.user.name,
     email: req.user.email,
   };
-  res.status(200).send(user);
+  res.status(200).json(user);
 });
 
 //@desc  Update user profile
 //@route PUT /api/users/profile
 //@access Private
 const updateUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).send({ message: 'Update user profile' });
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+    generateToken(res, updatedUser._id);
+    res.status(200).json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
 });
 
 export {
